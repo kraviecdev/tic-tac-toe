46035ada641277d04b7961adcbbd1c811e47c441
Patryk Krawczyk <patrick36212@gmail.com>
2022-03-10 18:52:51 +0100

    style change

--- src/Board/styled.js
+++ src/Board/styled.js
@@ -2,10 +2,11 @@ import styled from "styled-components";
 
 export const StyledBoard = styled.div`
     max-width: 100%;
-    margin: 10px;
+    margin: auto;
     padding: 5px;
 `
 
 export const StyledRow = styled.div`
-        display: table;
+        display: grid;
+        grid-auto-flow: column;
 `
