98073ea01e6081d2e3f2bedc205c992debb99523
Patryk Krawczyk <patrick36212@gmail.com>
2022-03-10 18:53:11 +0100

    delete unnecessary code

--- src/Game/styled.js
+++ src/Game/styled.js
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 
 export const StyledGame = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
+    max-width: fit-content;
+    margin: 0 auto;
 `
