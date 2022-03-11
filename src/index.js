73e37a6282defe5ae1b071214f5fe4e4f312ddb9
Patryk Krawczyk <patrick36212@gmail.com>
2022-03-11 12:35:18 +0100

    add Header to project

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from "react-dom";
 import Board from "./Board";
 import Game from "./Game";
 import { GlobalStyle } from "./GlobalStyle";
+import Header from "./Header";
 
 
 ReactDOM.render(
     <React.StrictMode>
         <GlobalStyle />
-            <Game children={
-                <Board />
-            } />
+        <Header />
+        <Game
+            children={<Board />}
+        />
     </React.StrictMode>,
     document.getElementById('root')
 );
