0f2852277937273ca5773976f69955c5df3bca91
Patryk Krawczyk <patrick36212@gmail.com>
2022-03-14 18:20:55 +0100

    delete styled component frmo main render

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from "react-dom";
 import Board from "./Board";
 import Game from "./Game";
 import { GlobalStyle } from "./GlobalStyle";
-import Header from "./Header";
 
 
 ReactDOM.render(
     <React.StrictMode>
         <GlobalStyle />
-        <Header />
         <Game
             children={<Board />}
         />
