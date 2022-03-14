df66071c77add241983842c54c2d30ada0d00fff
Patryk Krawczyk <patrick36212@gmail.com>
2022-03-14 18:21:44 +0100

    add disable attribiute to square after click

--- src/Board/index.js
+++ src/Board/index.js
@@ -5,23 +5,34 @@ import { StyledBoard, StyledRow } from "./styled";
 
 const Board = () => {
 
-    const [boardStatus, setBoardStatus] = useState(Array(9).fill(""));
-    const [player, setPlayer] = useState("X")
+    const [boardStatus, setBoardStatus] = useState(Array(9).fill(null));
+    const [disable, setDisable] = useState(Array(9).fill(false));
+    const [player, setPlayer] = useState("X");
+
 
     const chooseSquare = (i) => {
         setBoardStatus(
             boardStatus.map((value, id) => {
-                if (id === i && value === "") {
+                if (id === i && value === null) {
                     return player;
                 }
                 return value;
             })
         );
+
+        setDisable(
+            disable.map((disabled, id) => {
+                if ( id === i && disabled === false) {
+                    return true;
+                } return disabled;
+            })
+        );
+
         if (player === "X") {
             setPlayer("O")
         } else {
             setPlayer("X")
-        }
+        };
     };
 
     const calculateWinner = () => {
@@ -38,42 +49,51 @@ const Board = () => {
         <StyledBoard>
             <StyledRow>
                 <Square
+                    disabled={disable[0]}
                     onClick={() => chooseSquare(0)}
                     value={boardStatus[0]}
                 />
                 <Square
+                    disabled={disable[1]}
                     onClick={() => chooseSquare(1)}
                     value={boardStatus[1]}
                 />
                 <Square
+                    disabled={disable[2]}
                     onClick={() => chooseSquare(2)}
                     value={boardStatus[2]}
                 />
             </StyledRow>
             <StyledRow>
                 <Square
+                    disabled={disable[3]}
                     onClick={() => chooseSquare(3)}
                     value={boardStatus[3]}
                 />
                 <Square
+                    disabled={disable[4]}
                     onClick={() => chooseSquare(4)}
                     value={boardStatus[4]}
                 />
                 <Square
+                    disabled={disable[5]}
                     onClick={() => chooseSquare(5)}
                     value={boardStatus[5]}
                 />
             </StyledRow>
             <StyledRow>
                 <Square
+                    disabled={disable[6]}
                     onClick={() => chooseSquare(6)}
                     value={boardStatus[6]}
                 />
                 <Square
+                    disabled={disable[7]}
                     onClick={() => chooseSquare(7)}
                     value={boardStatus[7]}
                 />
                 <Square
+                    disabled={disable[8]}
                     onClick={() => chooseSquare(8)}
                     value={boardStatus[8]}
                 />
--- src/Board/styled.js
+++ src/Board/styled.js
@@ -4,9 +4,9 @@ export const StyledBoard = styled.div`
     max-width: 100%;
     margin: auto;
     padding: 5px;
-`
+`;
 
 export const StyledRow = styled.div`
         display: grid;
         grid-auto-flow: column;
-`
+`;
