144a579d95b2e0f6ff33f7f5179fe384fb790d89
Patryk Krawczyk <patrick36212@gmail.com>
2022-03-17 20:16:26 +0100

    add state result & setResult, playAgain functions

--- src/Board/index.js
+++ src/Board/index.js
@@ -9,7 +9,7 @@ const Board = () => {
     const [boardStatus, setBoardStatus] = useState(Array(9).fill(null));
     const [disable, setDisable] = useState(Array(9).fill(false));
     const [player, setPlayer] = useState("X");
-    // const [result, setResult] = useState(null);
+    const [result, setResult] = useState("");
 
     const chooseSquare = (i) => {
         setBoardStatus(
@@ -23,7 +23,7 @@ const Board = () => {
 
         setDisable(
             disable.map((disabled, id) => {
-                if ( id === i && disabled === false) {
+                if (id === i && disabled === false) {
                     return true;
                 } return disabled;
             })
@@ -34,75 +34,84 @@ const Board = () => {
         } else {
             setPlayer("X")
         };
+
+        setResult(() => {
+            for (let i = 0; i < Patterns.length; i++) {
+                const [a, b, c] = Patterns[i];
+                if (boardStatus[a] && boardStatus[a] === boardStatus[b] && boardStatus[a] === boardStatus[c]) {
+                    return boardStatus[a];
+                }
+            }
+            return null;
+        });
     };
 
-    const calculateWinner = () => {
-        for (let i = 0; i < Patterns.length; i++) {
-            const [a, b, c] = Patterns[i];
-            if (boardStatus[a] && boardStatus[a] === boardStatus[b] && boardStatus[a] === boardStatus[c]) {
-                return boardStatus[a];
-            }
-        }
-        return null;
+    const playAgain = () => {
+        setBoardStatus(Array(9).fill(null));
+        setDisable(Array(9).fill(false));
+        setPlayer("X")
+        setResult("")
     }
-
 
     return (
         <>
-        <StyledBoard>
-            <StyledRow>
-                <Square
-                    disabled={disable[0]}
-                    onClick={() => chooseSquare(0)}
-                    value={boardStatus[0]}
-                />
-                <Square
-                    disabled={disable[1]}
-                    onClick={() => chooseSquare(1)}
-                    value={boardStatus[1]}
-                />
-                <Square
-                    disabled={disable[2]}
-                    onClick={() => chooseSquare(2)}
-                    value={boardStatus[2]}
-                />
-            </StyledRow>
-            <StyledRow>
-                <Square
-                    disabled={disable[3]}
-                    onClick={() => chooseSquare(3)}
-                    value={boardStatus[3]}
-                />
-                <Square
-                    disabled={disable[4]}
-                    onClick={() => chooseSquare(4)}
-                    value={boardStatus[4]}
-                />
-                <Square
-                    disabled={disable[5]}
-                    onClick={() => chooseSquare(5)}
-                    value={boardStatus[5]}
-                />
-            </StyledRow>
-            <StyledRow>
-                <Square
-                    disabled={disable[6]}
-                    onClick={() => chooseSquare(6)}
-                    value={boardStatus[6]}
-                />
-                <Square
-                    disabled={disable[7]}
-                    onClick={() => chooseSquare(7)}
-                    value={boardStatus[7]}
-                />
-                <Square
-                    disabled={disable[8]}
-                    onClick={() => chooseSquare(8)}
-                    value={boardStatus[8]}
-                />
-            </StyledRow>
-        </StyledBoard>
-        <Result />
+            <StyledBoard>
+                <StyledRow>
+                    <Square
+                        disabled={disable[0]}
+                        onClick={() => chooseSquare(0)}
+                        value={boardStatus[0]}
+                    />
+                    <Square
+                        disabled={disable[1]}
+                        onClick={() => chooseSquare(1)}
+                        value={boardStatus[1]}
+                    />
+                    <Square
+                        disabled={disable[2]}
+                        onClick={() => chooseSquare(2)}
+                        value={boardStatus[2]}
+                    />
+                </StyledRow>
+                <StyledRow>
+                    <Square
+                        disabled={disable[3]}
+                        onClick={() => chooseSquare(3)}
+                        value={boardStatus[3]}
+                    />
+                    <Square
+                        disabled={disable[4]}
+                        onClick={() => chooseSquare(4)}
+                        value={boardStatus[4]}
+                    />
+                    <Square
+                        disabled={disable[5]}
+                        onClick={() => chooseSquare(5)}
+                        value={boardStatus[5]}
+                    />
+                </StyledRow>
+                <StyledRow>
+                    <Square
+                        disabled={disable[6]}
+                        onClick={() => chooseSquare(6)}
+                        value={boardStatus[6]}
+                    />
+                    <Square
+                        disabled={disable[7]}
+                        onClick={() => chooseSquare(7)}
+                        value={boardStatus[7]}
+                    />
+                    <Square
+                        disabled={disable[8]}
+                        onClick={() => chooseSquare(8)}
+                        value={boardStatus[8]}
+                    />
+                </StyledRow>
+            </StyledBoard>
+            <Result trigger={result} onClick={() => playAgain()} >
+                <h1>Wielki Zwycięzca</h1>
+                <h3>{result}</h3>
+            </Result>
         </>
     );
 }
