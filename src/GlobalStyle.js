5716dfa83ab3c1876addee0555b1d91263295f03
Patryk Krawczyk <patrick36212@gmail.com>
2022-03-21 17:31:19 +0100

    Add global color

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -3,6 +3,7 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
     html {
         box-sizing: border-box;  
+        color: #424656;
     }
 
     *, ::after, ::before {
