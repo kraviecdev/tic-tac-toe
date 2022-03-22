f1146b436e3d804834b138018bce2ec6713c03cd
Patryk Krawczyk <patrick36212@gmail.com>
2022-03-22 21:03:56 +0100

    add bg image

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,9 +1,18 @@
 import { createGlobalStyle } from "styled-components";
+import background from "./tic-tac-toe-bg-dark.png";
 
 export const GlobalStyle = createGlobalStyle`
     html {
+        background-color: black;
+    }
+    #root {
+        height: 98vh;
         box-sizing: border-box;  
         color: #424656;
+        background: url("${background}");
+        background-repeat: no-repeat;
+        background-size: contain;
+        background-position: top;
     }
 
     *, ::after, ::before {
