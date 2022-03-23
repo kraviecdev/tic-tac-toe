785e34329e1a01000a4d9458cc67c070cdb2ddd9
Patryk Krawczyk <patrick36212@gmail.com>
2022-03-23 21:31:07 +0100

    change in render mode

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Board from "./Board";
-import Footer from "./Footer";
-import Game from "./Game";
 import { GlobalStyle } from "./GlobalStyle";
-import Header from "./Header";
-
 
 ReactDOM.render(
     <React.StrictMode>
         <GlobalStyle />
-        <Header title={"Tic Tac Toe"} />
-        <Game
-            children={<Board />}
-        />
-        <Footer />
+        <Board />
     </React.StrictMode>,
     document.getElementById('root')
 );
