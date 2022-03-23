47de4a70f6e545ab3d00cb7b88f297967b14b223
Patryk Krawczyk <patrick36212@gmail.com>
2022-03-23 21:33:13 +0100

    html marks change

--- src/Board/styled.js
+++ src/Board/styled.js
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 
-export const StyledBoard = styled.div`
+export const StyledBoard = styled.main`
     max-width: 100%;
     padding: 5px;
 `;
 
-export const StyledRow = styled.div`
+export const StyledRow = styled.section`
         display: grid;
         grid-auto-flow: column;
 `;
