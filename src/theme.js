a80c349f096c99d00922a7b994a8c45d3e689704
Patryk Krawczyk <patrick36212@gmail.com>
2022-03-28 21:00:21 +0200

    theme change, add more contrast

--- src/theme.js
+++ src/theme.js
@@ -8,17 +8,23 @@ const breakpoints = {
 export const light = {
     breakpoints: breakpoints,
     colors: {
+        background: "#F3F4F5",
+        fontColor: "#0D0D0D",
+        buttonBackgorund: "#b3b3b3",
+        shadowOne: "#484848",
+        shadowTwo: "#ffffff",
+        popupBg: "#424040ec",
     },
 };
 
 export const dark = {
     breakpoints: breakpoints,
     colors: {
-        background: "#000000",
+        background: "#1A1A1A",
         fontColor: "#ffffff",
         buttonBackgorund: "#606060",
-        shadowOne: "#414141",
-        shadowTwo: "#7f7f7f",
+        shadowOne: "#525252",
+        shadowTwo: "#6e6e6e",
         popupBg: "#424040ec",
     },
 };
