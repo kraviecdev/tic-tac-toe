bdc83943399b9b35cc91f090d278bc70fc02294f
Patryk Krawczyk <patrick36212@gmail.com>
2022-03-29 20:00:38 +0200

    style change

--- src/Square/styled.js
+++ src/Square/styled.js
@@ -2,25 +2,21 @@ import styled from 'styled-components';
 
 export const StyledSquare = styled.button`
     cursor: pointer;
-    width: 33%;
-    height: fill;
-    margin: 0 5px;
+    width: 100%;
     font-size: 132px;
-    font-weight: bold;
-    transition: 1s;
-    border-radius: 15px;
     background: ${({theme}) => theme.colors.buttonBackgorund};
-    box-shadow:  1px 1px 10px ${({theme}) => theme.colors.shadowOne},
-                -1px -1px 10px ${({theme}) => theme.colors.shadowTwo};   
+    border: 2px solid ${({theme}) => theme.colors.optionBorder};
+    transition: 1s;
 
     &:focus, :hover{
         outline: none;
-        box-shadow: none;
+        filter: brightness(60%);
     }
 
     &:disabled{
-        box-shadow: inset 5px 5px 10px ${({theme}) => theme.colors.shadowOne},
-                    inset -5px -5px 10px ${({theme}) => theme.colors.shadowTwo};
         color: ${({theme}) => theme.colors.fontColor};
+        filter: brightness(85%)
     }
+
+    
 `;
