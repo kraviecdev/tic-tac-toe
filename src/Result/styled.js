f76ea739f06e683bf8b2b9314cc19938af744cb9
Patryk Krawczyk <patrick36212@gmail.com>
2022-03-29 20:03:12 +0200

    delete unnecesary code & style change

--- src/Result/styled.js
+++ src/Result/styled.js
@@ -16,29 +16,24 @@ export const StyledResultInner = styled.section`
     display: inherit;
     flex-direction: column;
     align-items: center;
-    position: relative;
-    padding: 16px;
-    width: 100%;
-    max-width: 400px;
+    padding: 15px;
     background-color: ${({ theme }) => theme.colors.background};
-    border-radius: 10px;
+    border-radius: 4px;
 `;
 
 export const StyledResultButton = styled.button`
     text-transform: uppercase;
-    font-size: 32px;
+    font-size: 24px;
     cursor: pointer;
     padding: 10px;
-    border-radius: 10px;
+    border-radius: 4px;
     transition-duration: 1s;
-    background: ${({ theme }) => theme.colors.buttonBackgorund};
-    box-shadow:  1px 1px 10px ${({ theme }) => theme.colors.shadowOne},
-                -1px -1px 10px ${({ theme }) => theme.colors.shadowTwo};   
+    background: ${({ theme }) => theme.colors.buttonBackgorund};  
     color: ${({ theme }) => theme.colors.fontColor};
 
     &:focus, :hover{
         outline: none;
-        filter: brightness(60%);
+        filter: brightness(70%);
     }
 `;
 
@@ -49,14 +44,14 @@ export const StyledResultTitle = styled.h1`
     overflow: hidden;
     white-space: nowrap;
     animation: 
-        typing 3.5s steps(35, end);
+        typing 2s steps(35, end);
 
     @keyframes typing {
         from {
             width: 0%;
             }
         to {
-            width: 73%;
+            width: 100%;
             }
         }
 
