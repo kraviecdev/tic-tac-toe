abf3b780c5dd0eb7cd99be7bb689db5b2985ed0e
Patryk Krawczyk <patrick36212@gmail.com>
2022-03-30 21:20:26 +0200

    Add themeSwitch cmponent

--- src/game.js
+++ src/game.js
@@ -4,14 +4,15 @@ import Header from './Header';
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from './GlobalStyle';
 import {dark, light} from './theme';
+import { ThemeSwitch } from './ThemeSwitch';
 
 
 function Game() {
 
     return (
-        <ThemeProvider theme={dark} >
+        <ThemeProvider theme={light} >
             <GlobalStyle />
-            <Header title={"Chuj do dupy"} />
+            <Header children={<ThemeSwitch />}/>
             <Board />
         </ThemeProvider>
     );
