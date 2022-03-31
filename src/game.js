99183f851fa060c94d5b40c621089b55f95f420a
Patryk Krawczyk <patrick36212@gmail.com>
2022-03-31 16:12:30 +0200

    create theme switch function

--- src/game.js
+++ src/game.js
@@ -1,18 +1,40 @@
 import Board from './Board';
 import Header from './Header';
 
+import { useState } from 'react';
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from './GlobalStyle';
-import {dark, light} from './theme';
+import { dark, light } from './theme';
 import { ThemeSwitch } from './ThemeSwitch';
 
 
 function Game() {
+    const [theme, setTheme] = useState('light')
+
+    const toggleTheme = () => {
+        if (theme === 'light') {
+            setTheme('dark')
+        } else {
+            setTheme('light')
+        }
+    };
+
+    const isDarkTheme = () => {
+        if (theme === 'dark') {
+            return true;
+        } return false;
+    };
 
     return (
-        <ThemeProvider theme={light} >
+        <ThemeProvider theme={theme === 'light' ? light : dark} >
             <GlobalStyle />
-            <Header children={<ThemeSwitch />}/>
+            <Header children={
+                <ThemeSwitch
+                    onClick={() => toggleTheme()}
+                    status={theme === 'light' ? "on" : "off"}
+                    active={isDarkTheme()}
+                />
+            } />
             <Board />
         </ThemeProvider>
     );
